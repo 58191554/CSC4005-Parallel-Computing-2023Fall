2e91d9717d885e2d6c7b724595ddce082c1da441
tongzhen <120090694@link.cuhk.edu.cn>
2023-10-23 00:16:51 -0400

    pj2 openmp -v=ptr slow

--- project2/src/openmp.cpp
+++ project2/src/openmp.cpp
@@ -20,93 +20,55 @@ Matrix matrix_multiply_openmp(const Matrix& matrix1, const Matrix& matrix2, int 
     size_t M = matrix1.getRows(), K = matrix1.getCols(), N = matrix2.getCols();
 
     Matrix result(M, N);
+    auto ** memM1 = (float**)malloc((M+16) * sizeof(float*));
+    auto ** memM2 = (float**)malloc((K+16) * sizeof(float*));
 
-    // Your Code Here!
-    // Optimizing Matrix Multiplication 
-    // In addition to SIMD, Memory Locality and Cache Missing,
-    // Further Applying OpenMp
-
-    // set tile size
-    size_t tile_size = 32;
-    std::cout << "M = " << M << ", N = "<<N << ", K = "<<K << std::endl;
-    std::cout << "tile_sizeM = " << tile_size 
-    << " tile_sizeN = " << tile_size
-    << " tile_sizeK = " << tile_size  << std::endl;
-
-    auto ** memM1 = (float**)malloc((M+tile_size) * sizeof(float*));
-    auto ** memM2 = (float**)malloc((K+tile_size) * sizeof(float*));
-
-    for(size_t i = 0; i < M+tile_size; i++){
+    for(size_t i = 0; i < M+16; i++){
         // std::cout << i << std::endl;
-        memM1[i] = (float*) malloc((K+tile_size)*sizeof(float));
+        memM1[i] = (float*) malloc((K+16)*sizeof(float));
         if(i < M){
-            for(size_t j = 0; j < K+8; j++){
+            for(size_t j = 0; j < K; j++){
                 // std::cout << j << std::endl;
-                if(j < K){
-                    memM1[i][j] = static_cast< float >(matrix1[i][j]);   
-                }         
+                memM1[i][j] = static_cast< float >(matrix1[i][j]);            
             }
         }
     }
-    for(size_t i = 0; i < K+tile_size; i++){
-        memM2[i] = (float*)malloc((N+8)*sizeof(float));
+    for(size_t i = 0; i < K+16; i++){
+        memM2[i] = (float*)malloc((N+16)*sizeof(float));
         if(i<K){
-            for(size_t j = 0; j < N+tile_size; j++){
-                if(j < N){
-                    memM2[i][j] = static_cast< float >(matrix2[i][j]);
-                }
+            for(size_t j = 0; j < N; j++){
+                memM2[i][j] = static_cast< float >(matrix2[i][j]);
             }
         }
     }
-    auto ** memresult = (float**)malloc((M+tile_size)*sizeof(float*));
-    for(size_t i = 0; i < M+8; i++){
-        memresult[i] = (float*)malloc((N+tile_size)*sizeof(float));
+    auto ** memresult = (float**)malloc((M+16)*sizeof(float*));
+    for(size_t i = 0; i < M+16; i++){
+        memresult[i] = (float*)malloc((N+16)*sizeof(float));
         for(size_t j = 0; j < N; j++){
             memresult[i][j] = 0.0f;
         }
     }
     
-    size_t numtile_M = M/tile_size+1;
-    size_t numtile_N = N/tile_size+1;
-    size_t numtile_K = K/tile_size+1;
-    // store arrays of tile sizes
-    int* tile_sizes_M = (int*)malloc(numtile_M*sizeof(int));
-    int* tile_sizes_N = (int*)malloc(numtile_N*sizeof(int));
-    int* tile_sizes_K = (int*)malloc(numtile_K*sizeof(int));
+    // divide task
+    int M_pad = (M%8 == 0) ? M : (M/8) * 8 + 8;
+    int N_pad = (N%8 == 0) ? N : (N/8) * 8 + 8;
+    std::cout << "M_pad, N_pad = " << M_pad << ", " << N_pad << std::endl;
+    int total_task_num = M_pad*N_pad / 64;
+    int block_rows_num = M_pad/8;
+    int block_cols_num = N_pad/8;
 
-    // get length for each valid tile
-    for(int i = 0; i < numtile_M; i++){
-        if(i < numtile_M-1) tile_sizes_M[i] = tile_size;
-        else tile_sizes_M[i] = M-i*tile_size;
-    }
-    for(int i = 0; i < numtile_N; i++){
-        if(i<numtile_N-1)tile_sizes_N[i] = tile_size;
-        else tile_sizes_N[i] = N-i*tile_size;
-    }
-    for(int i = 0; i < numtile_K; i++){
-        if(i < numtile_K-1) tile_sizes_K[i] = tile_size;
-        else tile_sizes_K[i] = K-i*tile_size;
-    }
+    int block_per_task = total_task_num/num_threads;
+    int left_block_num = total_task_num%num_threads;
 
-    // allocate the workload to each thread
-    int total_tile_num = numtile_M*numtile_N;
-    std::cout << "total tile number = " << total_tile_num<<std::endl;
-    int tile_num_per_task = total_tile_num/num_threads;
-    // squeeze the 2d tiles into 1d tiles allocate work
-    int left_tile_num = total_tile_num % num_threads;
-
-    std::vector<int> cuts(num_threads + 1, 0);
-    int divided_left_tile_num = 0;
+    std::vector <int> cuts(num_threads+1, 0);
+    int divided_left_block_num = 0;
     int actual_num_thread = 0;
 
     for (int i = 0; i < num_threads; ++i) {
-        if (divided_left_tile_num < left_tile_num) {
-            cuts[i+1] = cuts[i] + tile_num_per_task + 1;
-            divided_left_tile_num++;
-        } else cuts[i+1] = cuts[i] + tile_num_per_task;
-        // std::cout << "cuts[i] = " << cuts[i] << " cuts[i+1] = "<<
-        // cuts[i+1] << std::endl;
-        // compute actual num_thead
+        if (divided_left_block_num < left_block_num) {
+            cuts[i+1] = cuts[i] + block_per_task + 1;
+            divided_left_block_num++;
+        } else cuts[i+1] = cuts[i] + block_per_task;
         if(cuts[i]!=cuts[i+1]){
             actual_num_thread++;
         }
@@ -114,96 +76,98 @@ Matrix matrix_multiply_openmp(const Matrix& matrix1, const Matrix& matrix2, int 
     num_threads = actual_num_thread;
     std::cout << "actural threads number = " << num_threads << std::endl;
     omp_set_num_threads(num_threads);
+    auto start_time = std::chrono::high_resolution_clock::now();
 
-    #pragma omp parallel  shared(memM1, memM2, memresult, cuts, \
-    num_threads, numtile_N, numtile_K, tile_size, tile_sizes_M, tile_sizes_N, \
-    tile_sizes_K, result)
+    #pragma omp parallel  shared(memM1, memM2, memresult, cuts, M_pad, N_pad,\
+    num_threads, result, block_rows_num, block_cols_num)
     #pragma omp for
     for(int task_id = 0; task_id < num_threads; task_id++){
         // for each task, parse the linear task array to 2d tile index
         // std::cout << "taskid = " << task_id  <<std::endl;
+        // parse the block_id to 2d array
+        int block_num = cuts[task_id+1]-cuts[task_id];
 
-        for(int tile_id = cuts[task_id]; tile_id < cuts[task_id+1];tile_id++){
-            int ti = tile_id/numtile_N;
-            int tj = tile_id-ti*numtile_N;
-            // std::cout << "round info tile_id, ti, tj" << tile_id <<", "<<ti << ", " << tj << std::endl;
-            size_t row_offset = ti * tile_size; 
-            size_t col_offset = tj * tile_size;
+        for(int block_id = cuts[task_id]; block_id < cuts[task_id+1]; block_id++){
+            size_t block_row = (block_id/block_cols_num)*8;
+            size_t block_col = (block_id-block_row*block_cols_num/8)*8;
+            // std::cout << "block_id, block_row, block_col = " << block_id << ", "
+            //  << block_row << ", " << block_col << std::endl;
 
-            size_t lenM = tile_sizes_M[ti];
-            size_t lenN = tile_sizes_N[tj];
+            for(int x = 0; x < 8; x++){
+                // laod the row pointer of the mat1
+                auto mat1_row_ptr = memM1[block_row + x];
+                auto mem_result_row_ptr = memresult[block_row+x];
 
-            for(size_t tk = 0; tk < numtile_K; ++tk){
-                // do the tile matrix multiply for tile_num_K x tile_num_K times
-                size_t mid_offset = tk * tile_size;
+                // load the row pointer of M2 from k+0 to k+7 (k incrs by 8)
+                for(size_t k = 0; k < K; k += 8){
+                    auto mat2_ptr_k0 = memM2[k+0];
+                    auto mat2_ptr_k1 = memM2[k+1];
+                    auto mat2_ptr_k2 = memM2[k+2];
+                    auto mat2_ptr_k3 = memM2[k+3];
+                    auto mat2_ptr_k4 = memM2[k+4];
+                    auto mat2_ptr_k5 = memM2[k+5];
+                    auto mat2_ptr_k6 = memM2[k+6];
+                    auto mat2_ptr_k7 = memM2[k+7];
 
-                size_t lenK = tile_sizes_K[tk];
-                // std::cout << "tile info: ti, tj, tk = "<<ti << " " << tj << " "<< tk << std::endl;
-                // In each tile multiply compute divide into 2x2 vectorized compute
-                for(size_t x = 0; x < lenM; x+=8){
-                    for(size_t y = 0; y < lenN; y+=8){
-                        for(size_t i = 0; i < 8; i++){
-                            // take a 1x8 row and do vectorization and add vector to get a 1x8 result
-                            // take the next 1x8 row horizontaly and do the vector compute again
-                            for(size_t z = 0; z < lenK;z += 8){
-                                // std::cout << "x, y, z, i = " <<x<< ", "<<y<<", "<<z <<", "<<i << std::endl;
-                                // declear the registors
-                                __m256 row = _mm256_setzero_ps();
-                                __m256 k0, k1 ,k2, k3, k4, k5, k6, k7;
-                                k0 = _mm256_loadu_ps(&memM2[mid_offset+z+0][col_offset+y]);
-                                k1 = _mm256_loadu_ps(&memM2[mid_offset+z+1][col_offset+y]);
-                                k2 = _mm256_loadu_ps(&memM2[mid_offset+z+2][col_offset+y]);
-                                k3 = _mm256_loadu_ps(&memM2[mid_offset+z+3][col_offset+y]);
-                                k4 = _mm256_loadu_ps(&memM2[mid_offset+z+4][col_offset+y]);
-                                k5 = _mm256_loadu_ps(&memM2[mid_offset+z+5][col_offset+y]);
-                                k6 = _mm256_loadu_ps(&memM2[mid_offset+z+6][col_offset+y]);
-                                k7 = _mm256_loadu_ps(&memM2[mid_offset+z+7][col_offset+y]);
-                                row += memM1[row_offset+x+i][mid_offset+z+0] * k0;
-                                row += memM1[row_offset+x+i][mid_offset+z+1] * k1;
-                                row += memM1[row_offset+x+i][mid_offset+z+2] * k2;
-                                row += memM1[row_offset+x+i][mid_offset+z+3] * k3;
-                                row += memM1[row_offset+x+i][mid_offset+z+4] * k4;
-                                row += memM1[row_offset+x+i][mid_offset+z+5] * k5;
-                                row += memM1[row_offset+x+i][mid_offset+z+6] * k6;
-                                row += memM1[row_offset+x+i][mid_offset+z+7] * k7;
-                                // load the 1x8 result into sb
-                                float sb[8];
-                                // Load into a buffer and increase by the tile matrix value
-                                _mm256_store_ps(sb, row);
+                    __m256 row = _mm256_setzero_ps();
+                    __m256 kv0, kv1 ,kv2, kv3, kv4, kv5, kv6, kv7;
+                    kv0 = _mm256_loadu_ps(&mat2_ptr_k0[block_col]);
+                    kv1 = _mm256_loadu_ps(&mat2_ptr_k1[block_col]);
+                    kv2 = _mm256_loadu_ps(&mat2_ptr_k2[block_col]);
+                    kv3 = _mm256_loadu_ps(&mat2_ptr_k3[block_col]);
+                    kv4 = _mm256_loadu_ps(&mat2_ptr_k4[block_col]);
+                    kv5 = _mm256_loadu_ps(&mat2_ptr_k5[block_col]);
+                    kv6 = _mm256_loadu_ps(&mat2_ptr_k6[block_col]);
+                    kv7 = _mm256_loadu_ps(&mat2_ptr_k7[block_col]);   
 
-                                for(size_t e = 0; e < 8; e++){
-                                    memresult[row_offset+x+i][col_offset+y+e] += sb[e];
-                                }
-                            }
-                        }       
-                    }
+
+                    row += mat1_row_ptr[k+0] * kv0;
+                    row += mat1_row_ptr[k+1] * kv1;
+                    row += mat1_row_ptr[k+2] * kv2;
+                    row += mat1_row_ptr[k+3] * kv3;
+                    row += mat1_row_ptr[k+4] * kv4;
+                    row += mat1_row_ptr[k+5] * kv5;
+                    row += mat1_row_ptr[k+6] * kv6;
+                    row += mat1_row_ptr[k+7] * kv7;
+
+                    // load the 1x8 result into sb
+                    float sb[8];
+
+                    // Load into a buffer and increase by the tile matrix value
+                    _mm256_store_ps(sb, row);
+                    for(size_t e = 0; e < 8; e++){
+                        mem_result_row_ptr[block_col+e] += sb[e];
+                    }            
+                    
                 }
             }
         }
     }
+        
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_time - start_time);
+    std::cout << "MultiThread Time: " << elapsed_time.count() << " milliseconds"
+              << std::endl;
 
-    // load data
-    for(int i = 0; i < M; ++i){
-        for(int j = 0; j < N; ++j){
-            result[i][j] = memresult[i][j];
-            // std::cout << result[i][j] << " " ; 
+    std::cout <<"FUCK ME" << std::endl;
+    for(int i = 0; i < M; i++){
+        auto mem_result_ptr_i = memresult[i];
+        for(int j = 0; j < N; j++){
+            result[i][j] = mem_result_ptr_i[j];
         }
-        // std::cout <<std::endl;
     }
 
-    for(size_t i = 0; i < M+tile_size; i++){
+    for(size_t i = 0; i < M+16; i++){
         free(memM1[i]);
     }
     // transpose M2
-    for(size_t i = 0; i < K+tile_size; i++){
+    for(size_t i = 0; i < K+16; i++){
         free(memM2[i]);
     }
-    for(size_t i = 0; i < M+tile_size; i++){
+    for(size_t i = 0; i < M+16; i++){
         free(memresult[i]);
     }
-    delete[] tile_sizes_M;
-    delete[] tile_sizes_N;
-    delete[] tile_sizes_K;
     return result;
 }
 
