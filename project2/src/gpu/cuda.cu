1c834e5494a71f9996803f6c3a43f0347a535125
tongzhen <120090694@link.cuhk.edu.cn>
2023-10-25 10:24:48 -0400

    pj2 cuda framework

--- project2/src/gpu/cuda.cu
+++ project2/src/gpu/cuda.cu
@@ -4,3 +4,111 @@
 //
 // Matrix Multiplication with CUDA, for bonus
 //
+
+#include <iostream>
+#include <cuda_runtime.h>
+#include <stdexcept>
+#include <chrono>
+#include "matrix.hpp"
+#include <cstdlib>
+
+__global__ void matrix_multiply_cuda(int* A, int* B, int* C, int numRows, int numCols) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < numRows && col < numCols) {
+        int result = 0;
+        for (int i = 0; i < numCols; i++) {
+            result += A[row * numCols + i] * B[i * numCols + col];
+        }
+        C[row * numCols + col] = result;
+    }
+}
+
+int main(int argc, char** argv) {
+    // Verify input argument format
+    if (argc != 5) {
+        throw std::invalid_argument(
+            "Invalid argument, should be: ./executable "
+            "/path/to/matrix1 /path/to/matrix2 /path/to/multiply_result\n");
+    }
+
+    const std::string matrix1_path = argv[1];
+
+    const std::string matrix2_path = argv[2];
+
+    const std::string result_path = argv[3];
+
+    const int debug_flag = atoi(argv[4]);
+
+    Matrix matrix1 = Matrix::loadFromFile(matrix1_path);
+
+    Matrix matrix2 = Matrix::loadFromFile(matrix2_path);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    Matrix result = matrix_multiply_cuda(matrix1, matrix2);
+
+
+    // load the matrix
+
+    // Allocate device memory for A, B, and C using cudaMalloc
+
+    // Copy data from host to device using cudaMemcpy
+
+    // Define grid and block dimensions
+
+    // Launch the matrixMul kernel with <<<...>>> syntax
+
+    // Copy the result matrix C back from device to host
+
+    // Free device memory using cudaFree
+
+    // Display or use the result matrix C
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_time - start_time);
+
+    result.saveToFile(result_path);
+
+    std::cout << "Output file to: " << result_path << std::endl;
+
+    std::cout << "Multiplication Complete!" << std::endl;
+    std::cout << "Execution Time: " << elapsed_time.count() << " milliseconds"
+              << std::endl;
+
+    if (debug_flag == 1){
+        std::cout << "Debug Mode" << std::endl;
+        // DEBUG THE ANSWER CORRECTNESS
+        std::string ans_mat_path;
+        if(result.getRows() == 4) ans_mat_path = "results/answers/m12.txt";
+        if(result.getRows() == 128) ans_mat_path = "results/answers/m34.txt";
+        if(result.getRows() == 1024) ans_mat_path = "results/answers/m56.txt";
+        if(result.getRows() == 2048) ans_mat_path = "results/answers/m78.txt";
+        if(result.getRows() == 127) ans_mat_path = "results/answers/m910.txt";
+        if(result.getRows() == 1818) ans_mat_path = "results/answers/m1112.txt";
+        
+        std::cout << "ans_mat_path = " << ans_mat_path << std::endl;
+        Matrix matrix_ans = Matrix::loadFromFile(ans_mat_path);
+
+        bool correct = true;
+        for(size_t i = 0; i < result.getRows(); ++i){
+            if (correct){
+                for(size_t j = 0; j < result.getCols(); ++j){ 
+                    if(result[i][j] != matrix_ans[i][j]){
+                        std::cout << "wrong index i = "<<i<<" j = "<<j<<std::endl;
+                        std::cout << result[i][j] << " != " << matrix_ans[i][j]<<std::endl;
+                        correct = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if(correct){
+            std::cout << "Answer Correct!" << std::endl;
+        }
+        // DEBUG THE ANSWER CORRECTNESS ENDS
+    }
+    return 0;
+}
